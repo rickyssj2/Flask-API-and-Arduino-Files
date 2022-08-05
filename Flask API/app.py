ec863433ae839b791cb06e78db2c3d19d7d0276b
Aryan Pandey <aryanpandey048@gmail.com>
2022-08-05 15:27:52 +0530

    final commit

--- Flask API/app.py
+++ Flask API/app.py
@@ -1,7 +1,7 @@
 from email.mime import image
 from flask import Flask, jsonify, render_template, request, url_for
 import numpy as np
-from load_model2 import predict_shelflife_status
+from predict_shelflife_status_model import predict_shelflife_status
 import werkzeug
 import os
 import urllib.request
@@ -12,26 +12,9 @@ import pickle
 
 app = Flask(__name__)
 
-# def generator_for_data():
-#     for i in range(0,5):
-#         yield round(1.1 * np.random.randn() + 5, 2)
-
-# @app.before_request
-# def before():
-#     print("This is executed BEFORE each request.")
-
-temp_list = []
 image_list = []
 
-@app.route("/api", methods=["GET"])
-def api():
-    if(request.method == 'GET'):
-
-        global temp_list
-        response = jsonify(temp_list)
-
-        return response
-
+# this is to clear browser cache before each api call
 @app.after_request
 def add_header(r):
     r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
@@ -40,46 +23,8 @@ def add_header(r):
     r.headers['Cache-Control'] = 'public, max-age=0'
     return r
         
-@app.route('/add', methods =['POST'])
-def add_data_point():
-    if(request.method == 'POST'):
 
-        global temp_list
-        temp_list.append({"T": str(round(1.1 * np.random.randn() + 5, 2))})
-        return ''
-        
-@app.route('/reset', methods =['POST'])
-def reset_data():
-    if(request.method == 'POST'):
-        global temp_list
-        temp_list = []
-        return ''
-
-@app.route('/test', methods = ['POST','GET'])
-def test():
-    if(request.method == 'POST'):        
-        json = request.get_json()
-        imageURL = json['imageURL']
-        print(imageURL)
-        urllib.request.urlretrieve(imageURL,"test.png")  
-        img = Image.open("test.png")
-
-        #Predict weight loss status
-        weight_loss_status = predict_weightloss_status_img(img)
-        #Predict sprout status
-        sprout_status = predict_sprout_status_img(img)
-         #Predict disease status
-        disease_status = predict_disease_status_img(img)
-        return jsonify({
-            "weight loss status" : str(weight_loss_status),
-            "sprout status" : str(sprout_status),
-            "disease status" : str(disease_status),
-        })
-    else:
-        return """
-            <h1>make a post request with imageURL<h1>
-        """
-
+# THis is where the flutter app uploads the image for model running. Wifi module uploads the image tjo firebase and them the mobile app acesses the image and uploads to this url to run models on that image 
 @app.route('/upload', methods =['POST'])
 def upload_image():
     if(request.method == 'POST'):
@@ -100,7 +45,7 @@ def upload_image():
         image_list = []
         return jsonify({
             "message": "Image uploaded successfully",
-            "weight loss status" : str(weight_loss_status),
+            "weight loss status" : str(weight_loss_status), # this will give 0 or 1 as output
             "sprout status" : str(sprout_status),
             "disease status" : str(disease_status),
         })
@@ -113,7 +58,7 @@ def manage_your_stock():
         filename = werkzeug.utils.secure_filename(imageFile.filename)
         imageFile.save(os.path.join("static","images", filename))
 
-        #Manag your stock result
+        #Manage your stock result (The function is imported from load_model2.py file)
         result = predict_shelflife_status(os.path.join("static","images", filename))
 
         image_list.append(filename)
@@ -122,7 +67,17 @@ def manage_your_stock():
         image_list = []
         return jsonify({
             "message": "Image uploaded successfully",
-            "result": result
+            "result": result 
+            # the output is a python dict of the form {
+    #                                   "ambient":{
+    #                                           "upper_limit":20,
+    #                                            "lower_limit":15
+    #                                              },
+    #                                       "cold":{
+    #                                               "upper_limit":50,
+    #                                                                                                                                                                   "lower_limit":40
+    #                                                },
+    #                                       }
         })
 
         
@@ -159,7 +114,7 @@ def predict_weightloss_status(image_path):
     feature_vector = feature_vector.reshape(1,-1)
     import pickle
     # load saved model
-    with open('model.pkl' , 'rb') as f:
+    with open('model_weightloss_prediction.pkl' , 'rb') as f:
         model_load = pickle.load(f)
     y_pred = model_load.predict(feature_vector)
     return y_pred
@@ -177,7 +132,7 @@ def predict_weightloss_status_img(img):
     feature_vector = feature_vector.reshape(1,-1)
     import pickle
     # load saved model
-    with open('model.pkl' , 'rb') as f:
+    with open('model_weightloss_prediction.pkl' , 'rb') as f:
         model_load = pickle.load(f)
     y_pred = model_load.predict(feature_vector)
     return y_pred
